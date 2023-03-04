07a574092aebba3b557b5c2b98f3cad941979254
adnytzen02 <110806758+adnytzen02@users.noreply.github.com>
2023-03-04 22:42:23 +0800

    Update 03/04/2023

--- java.date.java
+++ java.date.java
@@ -1,53 +1,61 @@
-import java.time.LOCALDate;
-import java.time.LOCALTime;
-import java.time.LOCALDateTime;
-import java.time.DateTimeFormatter;
+// 使用的package
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
-
-public class javadate {
-    enum Day {
+public class JavaDate {
+    // 建立不可修改的常量
+    enum Day {  // enum是一個特殊的class，表示一組 constants(常量)
         LOCALDATE,
         LOCALTIME,
         LOCALDATETIME,
     }
 
     public static void main(String[] args) {
+        // 建立 Scanner 輸入
         Scanner input = new Scanner(System.in);
-        System.out.println("請輸入: ");
-        String newInput = input.nextline();
+        System.out.println("請輸入: <LOACLDATE/LOCALTIME/LOCALDATETIME> 其中一項");
+        
+        // 檢查 Scanner 類別中是否有可用的輸入。
+        String newInput = "";
+        if (input.hasNext()) {
+            newInput = input.next();
+        }
 
+        // 將輸入文字轉為大寫
         String upperInput = newInput.toUpperCase();
 
-        if(Day.LOCALDATE == upperInput) {
-            choice(LOCALDATE);
-        } else if (Day.LOCALTIME == upperInput) {
-            choice(LOCALTIME);
-        } else {
-            choice(LOCALDATETIME);
+        // 
+        if(Day.LOCALDATE.toString().equals(upperInput)) {
+            choice(Day.LOCALDATE);
+        } else if (Day.LOCALTIME.toString().equals(upperInput)) {
+            choice(Day.LOCALTIME);
+        } else if (Day.LOCALTIME.toString().equals(upperInput)) {
+            choice(Day.LOCALDATETIME);
         }
-
-        public static choice (String type) {
-            switch (type) {
-                case LOCALDATE:
-                    Date input_1 = new Date();
-                    input_1.getDate();
-                    break;
-                case LOCALTIME:
-                    Time input_2 = new Time();
-                    input_2.getTime();
-                    break;
-                case LOCALDATETIME:
-                    DateTime input_3 = new DateTime();
-                    input_3.getDateTime();
-                    break;
-            }
-        }
+        input.close();
     }
 
+    // 選擇查看哪一個 class
+    public static void choice(Day type) {
+        switch (type) {
+            case LOCALDATE:
+                Date.getDate();
+                break;
+            case LOCALTIME:
+                Time.getTime();
+                break;
+            case LOCALDATETIME:
+                DateTime.getDateTime();
+                break;
+        }
+    }
 }
+
 class Date {
-    // 當前日期
+    // 當前日期 class
     static void getDate() {
         LocalDate myObj = LocalDate.now();
         System.out.println(myObj);
@@ -55,7 +63,7 @@ class Date {
 }
 
 class Time {
-    // 當前時間
+    // 當前時間 class
     static void getTime() {
         LocalTime myObj = LocalTime.now();
         System.out.println(myObj);
@@ -63,9 +71,10 @@ class Time {
 }
 
 class DateTime {
-    // 當前日期-時間
+    // 當前日期-時間 class
     static void getDateTime() {
         LocalDateTime myObj = LocalDateTime.now();
+        // 將 日期-時間 格式化
         DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
         
         String formattedDate = myObj.format(myFormatObj);
