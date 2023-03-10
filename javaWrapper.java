b277dd58c40a7df141ca4c6b11413f2f5ae06473
adnytzen02 <110806758+adnytzen02@users.noreply.github.com>
2023-03-11 00:19:14 +0800

    Update 03/11/2023

---
+++ javaWrapper.java
@@ -0,0 +1,28 @@
+/*
+Wrapper class 內的物件(object)包含基本資料型類別(primitive data types)。
+Wrapper class創建一個物件時，它會包括一個變數而這個變數會包含一個資料型類別。
+
+作用:
+    透過將基本資料型類別轉做物件，可以將基本資料型類別(primitive data types)轉換成其他基本資料型類別。
+    將基本資料型類別轉做物件(Objects)，再應用到Collection framework (例如 ArrayList and Vector)。
+    在java.util package內，只可以處理(Objects) 。
+    可以在多執行緒的情況下支持同步。
+
+在Java中:
+    基本資料型類別(primitive data types)。
+    它們有各自對應的Wrapper Class。
+Primitive Data Type    <===>   Wrapper Class
+    byte    <===>   Byte
+    short   <===>   Short
+    int     <===>   Integer
+    long    <===>   Long
+    float   <===>   Float
+    double  <===>   Double
+    boolean <===>   Boolean
+    char    <===>   Character
+*/
+
+
+public class Main {
+    
+}
