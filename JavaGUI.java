15a96e0f96909f7e91ca8eab8e1df76d3ca984c0
adnytzen02 <110806758+adnytzen02@users.noreply.github.com>
2023-03-29 17:52:19 +0800

    Update 03/29/2023

--- JavaGUI.java
+++ JavaGUI.java
@@ -10,8 +10,29 @@ public class JavaGUI  extends JPanel {
 
 
     @Override
+    // Java calls the paintComponent method when it needs to.
     public void paintComponent(Graphics g) {
-        g.drawString("Hello world", x = 10, y = 10);
+        // 有先後順序
+        // g.drawString("Hello world", x = 10, y = 10); //顯示字串
+
+        // 獲取當前視窗大小
+        int width = getWidth();
+        int height = getHeight();
+
+        // 設定圖片
+        ImageIcon img = new ImageIcon(filename);
+
+        g.setColor(Color.BLACK); // 長方形的顏色
+        g.fillRect(100,100,width,height); // 畫長方形(X座標,y座標,寬度,長度)、(drawRect 空心)
+
+        g.setColor(Color.RED); // 線的顏色
+        g.drawLine(10,10,50,50); // 劃一條線
+
+        g.setColor(Color.BLUE);
+        g.fillOval(0, 0, 100, 100); // 劃一個半徑為10的正圓，darwOval是空心的圓
+
+        // 顯示圖片
+        img.paintIcon(null, g, 50, 50);
     }
 
     public static void main(String[] args) {
