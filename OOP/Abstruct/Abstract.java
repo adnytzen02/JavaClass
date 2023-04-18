c9ec1aae92b26220e9faf328d2590b82393e06cd
adnytzen02 <110806758+adnytzen02@users.noreply.github.com>
2023-04-18 23:24:47 +0800

    Update 04/18/2023

---
+++ OOP/Abstruct/Abstract.java
@@ -0,0 +1,9 @@
+public class Abstract {
+    public static void main(String[] args) {
+        Animal cat1 = new Cat();
+        Animal dog1 = new Dog();
+
+        cat1.makeSound();
+        dog1.makeSound();
+    }
+}
