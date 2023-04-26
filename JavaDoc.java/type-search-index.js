b5948e010e21c3de46f3a3836b66ce0d369261e7
adnytzen02 <110806758+adnytzen02@users.noreply.github.com>
2023-04-26 16:05:40 +0800

    Update 4/

--- JavaDoc.java/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"example","l":"Circle"},{"p":"example","l":"Main"},{"p":"example","l":"Nationality"},{"p":"example","l":"Person"}];updateSearchResults();
