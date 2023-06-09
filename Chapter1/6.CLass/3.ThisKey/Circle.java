ee9d84ce6521aa5295bbad8abae9f222a649fbd9
adnytzen02 <110806758+adnytzen02@users.noreply.github.com>
2023-06-10 00:29:23 +0800

    Update 06/10/2023

---
+++ Chapter1/6.CLass/3.ThisKey/Circle.java
@@ -0,0 +1,18 @@
+public class Circle {
+    private double radius;
+
+    // 無參數建構函式，呼叫帶參數建構函式並將半徑設定為預設值 1.0
+    public Circle() {
+        this(1.0);
+    }
+
+    // 帶參數建構函式，初始化半徑
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    // 取得圓的面積
+    public double getArea() {
+        return Math.PI * this.radius * this.radius;
+    }
+}
---
+++ Chapter1/6.CLass/3.ThisKey/ThisKey.java
@@ -0,0 +1,6 @@
+public class ThisKey {
+    public static void main(String[] args) {
+        Circle c1 = new Circle(10);  // 創建一個半徑為 10 的 Circle 對象
+        c1.getArea();  // 呼叫 getArea 方法，計算並忽略返回值
+    }
+}
